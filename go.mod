7ef7e49a7afbfa941ab983af0815f0f11a4ad21c
opas-keat <opas.keat@gmail.com>
2020-11-11 16:19:53 +0700

    update add api
    update add api

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.15
 
 require (
 	github.com/andybalholm/brotli v1.0.1 // indirect
-	github.com/gofiber/fiber/v2 v2.1.4 // indirect
+	github.com/gofiber/fiber/v2 v2.1.4
 	github.com/klauspost/compress v1.11.2 // indirect
 	golang.org/x/sys v0.0.0-20201109165425-215b40eba54c // indirect
 )
--- main.go
+++ main.go
@@ -4,16 +4,30 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/proxy"
 )
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
+	app.Use("/beisApi", proxy.Balancer(proxy.Config{
+		Servers: []string{
+			"http://10.5.46.116:8002",
+		},
+		ModifyRequest: func(c *fiber.Ctx) error {
+			c.Set("X-Real-IP", c.IP())
+			return c.Redirect(c.OriginalURL())
+		},
+	}))
+
+	app.Static("/", "./public", fiber.Static{
+		Compress:  true, // default: false
+		ByteRange: true, // default: false
 	})
 
-	app.Static("/", "./public")
+	app.Get("/*", func(ctx *fiber.Ctx) error {
+		return ctx.SendFile("./public/index.html")
+	})
 
 	log.Fatal(app.Listen(":3000"))
 
