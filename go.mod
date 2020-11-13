21367352832278ecba6372906b8625ec9cf43e00
opas-keat <opas.keat@gmail.com>
2020-11-13 09:19:08 +0700

    ีupdate report

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/andybalholm/brotli v1.0.1 // indirect
 	github.com/gofiber/fiber/v2 v2.1.4
+	github.com/gofiber/helmet/v2 v2.0.1
 	github.com/klauspost/compress v1.11.2 // indirect
-	golang.org/x/sys v0.0.0-20201109165425-215b40eba54c // indirect
+	golang.org/x/sys v0.0.0-20201112073958-5cba982894dd // indirect
 )
--- main.go
+++ main.go
@@ -2,17 +2,48 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
+	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/proxy"
+	"github.com/gofiber/helmet/v2"
 )
 
 func main() {
 	app := fiber.New()
 
+	app.Use(helmet.New())
+	// app.Use(csrf.New())
+
+	// Provide a custom compression level
+	app.Use(compress.New(compress.Config{
+		Level: compress.LevelBestSpeed, // 1
+	}))
+
+	// Or extend your config for customization
+	app.Use(cache.New(cache.Config{
+		Expiration:   30 * time.Minute,
+		CacheControl: true,
+	}))
+
 	app.Use("/pxapi_ect", proxy.Balancer(proxy.Config{
 		Servers: []string{
 			"http://172.17.8.88:8080",
+			"http://172.17.8.88:8081",
+			"http://172.17.8.88:8082",
+			"http://172.17.8.88:8083",
+		},
+		ModifyRequest: func(c *fiber.Ctx) error {
+			c.Set("X-Real-IP", c.IP())
+			return c.Redirect(c.OriginalURL())
+		},
+	}))
+
+	app.Use("/report", proxy.Balancer(proxy.Config{
+		Servers: []string{
+			"http://172.17.8.87:8080/jasperserver/",
 		},
 		ModifyRequest: func(c *fiber.Ctx) error {
 			c.Set("X-Real-IP", c.IP())
